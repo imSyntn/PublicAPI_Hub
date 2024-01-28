d4f2b0130f38ad146cd09a16ef421f274487a4c6
Sayantan Sarkar <imsyntn@gmail.com>
2024-01-28 16:18:06 +0530

    service worker testing

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import SmoothScroll from './Utils/SmoothScroll.jsx';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <SmoothScroll >
       <App />
      </SmoothScroll>
 )
+serviceWorkerRegistration.unregister();
