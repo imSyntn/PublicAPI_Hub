b126b266f698a83f1af51acbb1b744c1b70ba55b
Sayantan Sarkar <imsyntn@gmail.com>
2024-02-13 21:34:07 +0530

    framer-motion

--- src/Components/ResultCards.jsx
+++ src/Components/ResultCards.jsx
@@ -1,9 +1,10 @@
-import React, { memo, useContext } from 'react'
-import '../Styles/ResultCards.scss'
-import '../Styles/Misc.scss'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-import { ThemeContext } from '../App'
+import React, { memo, useContext } from "react";
+import "../Styles/ResultCards.scss";
+import "../Styles/Misc.scss";
+import { motion } from "framer-motion";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import { ThemeContext } from "../App";
 import { MdOutlineVpnKey } from "react-icons/md";
 import { FaLock } from "react-icons/fa";
 import { FaLockOpen } from "react-icons/fa";
@@ -11,83 +12,106 @@ import { TbShare } from "react-icons/tb";
 import { GrLinkNext } from "react-icons/gr";
 
 const ResultCards = memo(({ name, auth, Cors, desc, https, link }) => {
-
-  const { darkMode } = useContext(ThemeContext)
+  const { darkMode } = useContext(ThemeContext);
 
   return (
-    <div className={`ResultCards ${!darkMode ? 'cardInLight' : 'cardInDark'}`}>
+    <motion.div initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{delay: 0.1}} className={`ResultCards ${!darkMode ? "cardInLight" : "cardInDark"}`}>
       <div className="logoImg">
-        {
-          name ? (
-            <img src={`https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${link}&size=128`} alt="" />
-          ) : (
-            <Skeleton style={{
-              width: '45px',
-              height: '45px',
-              marginRight: '10px'
-            }} />
-          )
-        }
-        <h2>{name || <Skeleton style={{
-          width: '210px',
-          height: '30px'
-        }} />}</h2>
+        {name ? (
+          <img
+            src={`https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${link}&size=128`}
+            alt=""
+          />
+        ) : (
+          <Skeleton
+            style={{
+              width: "45px",
+              height: "45px",
+              marginRight: "10px",
+            }}
+          />
+        )}
+        <h2>
+          {name || (
+            <Skeleton
+              style={{
+                width: "210px",
+                height: "30px",
+              }}
+            />
+          )}
+        </h2>
       </div>
       <p>{desc || <Skeleton count={2} />}</p>
       <div className="options">
         <div className="auth cardSvgDiv">
-          {
-            name ? (
-              <><MdOutlineVpnKey /> <span>{auth}</span></>
-            ) : (
-              <Skeleton style={{
-                width: '50px',
-                marginLeft: '0px'
-              }} />
-            )
-          }
-          <p className='tooltip'>Authentication</p>
+          {name ? (
+            <>
+              <MdOutlineVpnKey /> <span>{auth}</span>
+            </>
+          ) : (
+            <Skeleton
+              style={{
+                width: "50px",
+                marginLeft: "0px",
+              }}
+            />
+          )}
+          <p className="tooltip">Authentication</p>
         </div>
         <div className="cors cardSvgDiv">
-          {
-            name ? (
-              <><TbShare /> <span>{Cors}</span></>
-            ) : (
-              <Skeleton style={{
-                width: '50px'
-              }} />
-            )
-          }
-          <p className='tooltip'>CORS</p>
+          {name ? (
+            <>
+              <TbShare /> <span>{Cors}</span>
+            </>
+          ) : (
+            <Skeleton
+              style={{
+                width: "50px",
+              }}
+            />
+          )}
+          <p className="tooltip">CORS</p>
         </div>
         <div className="https cardSvgDiv">
-          {
-            name ? (
-              <div className="security">
-                {https ? <><FaLock /> <span>HTTPS</span></> : <><FaLockOpen /> <span>HTTP</span></>}
-              </div>
-            ) : (
-              <Skeleton style={{
-                width: '50px'
-              }} />
-            )
-          }
-          <p className='tooltip'>HTTPS</p>
+          {name ? (
+            <div className="security">
+              {https ? (
+                <>
+                  <FaLock /> <span>HTTPS</span>
+                </>
+              ) : (
+                <>
+                  <FaLockOpen /> <span>HTTP</span>
+                </>
+              )}
+            </div>
+          ) : (
+            <Skeleton
+              style={{
+                width: "50px",
+              }}
+            />
+          )}
+          <p className="tooltip">HTTPS</p>
         </div>
-        <a href={link} target='_blank' className="link cardSvgDiv">
-          {
-            name ? (
-              <><p>Link </p><GrLinkNext /></>
-            ) : (
-              <Skeleton style={{
-                width: '50px'
-              }} />
-            )
-          }
+        <a href={link} target="_blank" className="link cardSvgDiv">
+          {name ? (
+            <>
+              <p>Link </p>
+              <GrLinkNext />
+            </>
+          ) : (
+            <Skeleton
+              style={{
+                width: "50px",
+              }}
+            />
+          )}
         </a>
       </div>
-    </div>
-  )
-})
+    </motion.div>
+  );
+});
 
-export default ResultCards
+export default ResultCards;
