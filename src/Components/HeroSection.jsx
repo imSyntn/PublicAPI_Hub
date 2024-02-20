853bdfa590e23dce55ddb50e3a81592a06ad0dd4
Sayantan Sarkar <imsyntn@gmail.com>
2024-02-20 18:54:34 +0530

    Hero section text effect

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,13 +1,25 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState, useCallback } from 'react'
 import '../Styles/HeroSection.scss'
 import '../Styles/Misc.scss'
 
 const HeroSection = memo(({ theme }) => {
+    const [text, setText] = useState([])
+    const splitText = 'More than 1400+ public APIs to power-up your Project.'.split('');
+
+    useEffect(()=>{
+        const txtAnimateStart = async () => {
+            for(let i = 0; i<splitText.length; i++){
+                await new Promise(resolve => setTimeout(resolve,50))
+                    setText((prev)=> [...prev, splitText[i]])
+            }
+        }
+        txtAnimateStart()
+    },[])
 
     return (
         <div className="heroSection">
-            <h1 className={!theme ? 'h1InLightMode' : 'h1InDarkMode'}>Explore</h1>
-            <p className={!theme ? '' : 'textInDarkMode'}>More than 1400+ public APIs to power-up your Project.</p>
+            <h1>Explore</h1>
+            <p className={!theme ? '' : 'textInDarkMode'}>{text.join('')}</p>
         </div>
     )
 })
